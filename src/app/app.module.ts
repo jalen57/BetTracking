19ad2a9921ebfec3ee2da66f53a7588913718c46
Mathew Broeze <mjbroeze@gmail.com>
2021-03-30 02:26:29 -0400

    wager-form complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { ReactiveFormsModule } from '@angular/forms'
  * material imports
  * import {} from '@angular/material'
  */
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatButtonToggleModule } from '@angular/material/button-toggle'
-import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +34,15 @@ import { WagerFormComponent } from './wager-form/wager-form.component';
     ReactiveFormsModule,
     MatSidenavModule,
     MatCardModule,
-    MatButtonToggleModule
+    MatGridListModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/wager-form/wager-form.component.ts
+++ src/app/wager-form/wager-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-wager-form',
@@ -6,10 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wager-form.component.css']
 })
 export class WagerFormComponent implements OnInit {
+  wagerForm: FormGroup;
 
-  constructor() { }
+  leagueNames: Array<string> = ['MLB', 'NBA', 'NFL', 'NHL'];
+  betTypes: Array<string> = ['Point Spread', 'Money Line', 'Total'];
+  teams: Array<string> = ['Home', 'Away'];
+  periods: Array<string> = [];
+  results: Array<string> = ['Win', 'Push', 'Loss']
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.wagerForm = this.formBuilder.group({
+      league: '',
+      betType: '',
+      team: null,
+      period: '',
+      line: null,
+      odds: 100,
+      amount: 1,
+      inProgress: false,
+      result: null,
+      grossReturn: null,
+      netReturn: null
+    });
+
+    this.onChanges();
+  }
+
+  onChanges(): void {
+    this.wagerForm.get('league').valueChanges.subscribe(val => {
+      if (val == 'MLB') {
+        this.periods = ['Full Game', 'First5'];
+      }
+      else if (val == 'NHL') {
+        this.periods = ['Full Game', 'P1', 'P2', 'P3'];
+      }
+      else if (val == 'NBA') {
+        this.periods = ['Full Game', 'First Half', 'Second Half', 'Q1', 'Q2', 'Q3', 'Q4']
+      }
+      else if (val == 'NFL') {
+        this.periods = ['Full Game', 'First Half', 'Second Half', 'Q1', 'Q2', 'Q3', 'Q4']
+      }
+    });
+
+    this.wagerForm.get('betType').valueChanges.subscribe(val => {
+      if (val == 'Total') {
+        this.wagerForm.get('team').patchValue(null);
+        this.wagerForm.get('team').disable();
+
+        this.wagerForm.get('line').enable();
+      }
+      else if (val == 'Money Line') {
+        this.wagerForm.get('team').enable();
+
+        this.wagerForm.get('line').patchValue(null);
+        this.wagerForm.get('line').disable();
+      }
+      else {
+        this.wagerForm.get('team').enable();
+        this.wagerForm.get('line').enable();
+      }
+    });
+
+    this.wagerForm.get('inProgress').valueChanges.subscribe(val => {
+      if (val) {
+        this.wagerForm.get('result').patchValue(null);
+        this.wagerForm.get('result').disable();
+      }
+      else {
+        this.wagerForm.get('result').enable();
+      }
+    });
   }
 
 }
